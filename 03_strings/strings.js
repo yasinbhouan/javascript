0ea39cb01532f9ea7e410de06aa1a9570d681d26
Yasin Bhouan <yasinbhouan@gmail.com>
2025-08-11 21:21:13 +0600

    03_strings

---
+++ 03_strings/strings.js
@@ -0,0 +1,26 @@
+//strings
+'single quotes';
+"double quotes";
+
+//string with quotes
+'single quotes with "double quotes" inside';
+"double quotes with 'single quotes' inside";
+
+//string error with same quotes inside
+'single quotes with 'single quotes' inside'; // This will cause an error
+"double quotes with "double quotes" inside"; // This will also cause an error
+
+//escaping characters
+'It\'s a string with an escaped single quote'; // Escaping single quotes
+"She said, \"Hello!\""; // Escaping double quotes
+'Backslash can also be escaped: \\ This is a backslash'; // Escaping backslash
+
+//template strings
+`template strings`;
+`template strings can have "double quotes" and 'single quotes' inside them`;
+
+//template strings allow multiline strings
+`This is a template string
+that spans multiple lines`;
+
+'multiple' + 'string' + ' concatenation'; // Concatenating strings
