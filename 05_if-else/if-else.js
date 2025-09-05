5770c99e475b60f0bdd18623c4bc510e7848f3dc
Yasin Bhouan <yasinbhouan@gmail.com>
2025-09-05 21:02:22 +0600

    if else alternative

--- 05_if-else/if-else.js
+++ 05_if-else/if-else.js
@@ -35,3 +35,27 @@ if (false) {
 }else{
   console.log('This code will work'); // this code will run
 }
+
+// ternary operator
+true ? 'truthy' : 'falsy'; // returns 'truthy'
+false ? 'truthy' : 'falsy'; // returns 'falsy'
+
+// ternary oparator can be store in a variable
+let trueValue = true ? 'truthy' : 'falsy'; // 'truthy'
+let falseValue = false ? 'truthy' : 'falsy'; // 'falsy'
+console.log(trueValue);
+console.log(falseValue);
+
+//gaurd oparator, Logical AND (&&)
+let isLoggedIn = true;
+isLoggedIn && console.log('user is logged in'); // 'user is logged in' if isLoggedIn is true
+
+// Logical OR (||)
+let name = undefined; // '' null, undefined, 0, false
+let defaultName = name || 'guest user'; // 'guest user' if name is falsy
+console.log(defaultName);
+
+// default oparator, nullish coalescing (??)
+let username = undefined; // null or undefined.
+let defaultUsername = username ?? 'guest username'; // 'guest username' if username is falsy
+console.log(defaultUsername);
